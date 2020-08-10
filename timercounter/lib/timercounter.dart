e79e014fae8dab51cfa49f1cd63e50f06c89d3e5
邹琳 <774249737@qq.com>
2020-08-10 10:02:56 +0800

    版本提交

--- timercounter/lib/timercounter.dart
+++ timercounter/lib/timercounter.dart
@@ -6,32 +6,7 @@ class TimerCounter {
   /// 定时速度
   int _speed;
   Function(int count) callback;
-
-  /**
-    * Examples:
-
-    *     class _SmsCounterViewState extends State<SmsCounterView> {
-    *       TimeCounter _timer;
-    *       int counter = 59;
-
-    *     @override
-    *     void initState() {
-    *       super.initState();
-    *       _beginTimer();
-    *     }
-
-    *     @override
-    *     void dispose() {
-    *       _timer.stop();
-    *       super.dispose();
-    *     }
-
-    *     void _beginTimer() {
-    *       _timer = TimeCounter.begin(duration: 59, callback:(timerCount){ 
-    *         // do somthing ...
-    *       });
-    *     }
-    */
+  
   static TimerCounter begin({int duration, int speed = 1, Function(int count) callback}) {
     final tool = TimerCounter();
     tool.duration = duration;
